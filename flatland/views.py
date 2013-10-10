5bd36345db098ac5ff480666dc65a27f9a36aa4e
pavelsof <pavelsof@gmail.com>
2013-10-10 13:45:33 +0300

    Flatland has its first view.

--- flatland/views.py
+++ flatland/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.shortcuts import RequestContext
 
-# Create your views here.
+import json
+
+
+def start(request):
+	"""Perhaps the only view of the flatland app."""
+	data = {}
+
+	data['mode'] = '2d'
+
+	return render_to_response( 	'flatland.html',
+								{
+									'data': json.dumps(data)
+								},
+								context_instance = RequestContext(request) )
