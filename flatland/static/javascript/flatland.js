2cfc7f59b8d661bcd79245fbd051228a197c74c7
pavelsof <pavelsof@gmail.com>
2013-10-10 16:41:48 +0300

    Spaces to tabs.

--- flatland/static/javascript/flatland.js
+++ flatland/static/javascript/flatland.js
@@ -1,29 +1,29 @@
 var d = {
 
-  mode: "2d",
-  character: {
-    coord: [[10,10],[20,20],[30,30]]
-  },
-  triangle: {
-    coord: [[10,10],[20,20],[30,30]]
-  }
+	mode: "2d",
+	character: {
+		coord: [[10,10],[20,20],[30,30]]
+	},
+	triangle: {
+		coord: [[10,10],[20,20],[30,30]]
+	}
 
 };
 
 var visualizeShape = function(shape) {
-  console.log(shape.coord);
+	console.log(shape.coord);
 }
 
 visualizeShape(d.character);
 
 $(document).ready(function() {
 
-  var paper = Raphael(10, 10, 500, 500);
-  var circle = paper.circle(50, 40, 10);
-  circle.attr("fill", "#f00");
-  circle.attr("stroke", "#fff");
+	var paper = Raphael(10, 10, 500, 500);
+	var circle = paper.circle(50, 40, 10);
+	circle.attr("fill", "#f00");
+	circle.attr("stroke", "#fff");
 
-  var coord = [10,10,50,50]
-  var c = paper.rect(10,10,50,50);
+	var coord = [10,10,50,50]
+	var c = paper.rect(10,10,50,50);
 
 });
