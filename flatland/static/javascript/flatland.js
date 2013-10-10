0e938ac6e097ed1d4cce7733fc26e164f809d298
jsibelius <c_c@abv.bg>
2013-10-10 15:50:02 +0300

    added custom css file and some basic js structure

--- flatland/static/javascript/flatland.js
+++ flatland/static/javascript/flatland.js
@@ -1,1 +1,18 @@
-console.log(2222);
+var d = {
+
+  mode: "2d",
+  character: {
+    coord: [[10,10],[20,20],[30,30]]
+  },
+  triangle: {
+    coord: [[10,10],[20,20],[30,30]]
+  }
+
+};
+
+var visualizeShape = function(shape) {
+  console.log(shape.coord);
+}
+
+visualizeShape(d.character);
+
